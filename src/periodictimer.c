42f3487512200f3125bb55cbe0289149032667e1
Fabian <fabian.graf@siemens.com>
2023-10-12 08:08:54 +0200

    Fixed periodic_timer input variable

--- src/periodictimer.c
+++ src/periodictimer.c
@@ -39,33 +39,7 @@ void periodictimer_init(uint16_t period_s, periodtimer_cbt periodtimer_cb) {
     // initialize timer
     k_timer_init(&periodic_zephyr_timer, RTC0_IRQHandler, NULL);
     periodictimer_vars.countdown_s     = periodictimer_vars.period_s;
-    k_timer_start(&periodic_zephyr_timer, K_MSEC(1000), K_MSEC(1000));
-
-    /*
-    // store params
-    periodictimer_vars.period_s        = period_s;
-    periodictimer_vars.periodtimer_cb  = periodtimer_cb;
-
-    // configure/start the RTC
-    // 1098 7654 3210 9876 5432 1098 7654 3210
-    // xxxx xxxx xxxx FEDC xxxx xxxx xxxx xxBA (C=compare 0)
-    // 0000 0000 0000 0001 0000 0000 0000 0000 
-    //    0    0    0    1    0    0    0    0 0x00010000
-    NRF_RTC0->EVTENSET                 = 0x00010000;       // enable compare 0 event routing
-    NRF_RTC0->INTENSET                 = 0x00010000;       // enable compare 0 interrupts
-
-    // enable interrupts
-    NVIC_SetPriority(RTC0_IRQn, 1);
-    NVIC_ClearPendingIRQ(RTC0_IRQn);
-    NVIC_EnableIRQ(RTC0_IRQn);
-
-    // prepare the countdown
-    periodictimer_vars.countdown_s     = periodictimer_vars.period_s;
-    
-    // have RTC tick every second
-    NRF_RTC0->CC[0]                    = 32768;            // 32768 = 1 s
-    NRF_RTC0->TASKS_START              = 0x00000001;       // start RTC0
-    */
+    k_timer_start(&periodic_zephyr_timer, K_MSEC(period_s*1000), K_MSEC(period_s*1000));
 }
 
 //=========================== private =========================================
@@ -73,30 +47,8 @@ void periodictimer_init(uint16_t period_s, periodtimer_cbt periodtimer_cb) {
 //=========================== interrupt handlers ==============================
 
 void RTC0_IRQHandler(void) {
-
-    // debug
-    periodictimer_dbg.num_ISR_RTC0_IRQHandler++;
-    /*
-    // handle compare[0]
-    if (NRF_RTC0->EVENTS_COMPARE[0] == 0x00000001 ) {
-
-        // clear flag
-        NRF_RTC0->EVENTS_COMPARE[0]    = 0x00000000;
-
-        // clear COUNTER
-        NRF_RTC0->TASKS_CLEAR          = 0x00000001;
-
-        // debug
-        periodictimer_dbg.num_ISR_RTC0_IRQHandler_COMPARE0++;
-
-        // handle
-        periodictimer_vars.countdown_s--;
-        if (periodictimer_vars.countdown_s==0) {
-            periodictimer_vars.countdown_s = periodictimer_vars.period_s;
-            periodictimer_vars.periodtimer_cb();
-        }
-    }
-    */
+   // debug
+   periodictimer_dbg.num_ISR_RTC0_IRQHandler++;
    periodictimer_vars.countdown_s--;
    periodictimer_vars.countdown_s = periodictimer_vars.period_s;
    periodictimer_vars.periodtimer_cb();
